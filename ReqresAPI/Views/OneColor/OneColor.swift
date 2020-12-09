6fcd5fd68b6ce15313a472cf5994e66a5c894d4b
Shamil Rakhimov <srmufa97@gmail.com>
2020-12-09 15:35:59 +0300

    update oneColor view

--- ReqresAPI/Views/OneColor/OneColor.swift
+++ ReqresAPI/Views/OneColor/OneColor.swift
@@ -20,20 +20,39 @@ struct OneColor: View {
             Spacer()
             VStack {
                 Spacer()
-                Text("\(self.reqresColor.name)").bold()
+                Text("\(self.reqresColor.name)").font(.title).bold()
                 Text("\(String(self.reqresColor.year))")
                 Text("\(self.reqresColor.code)")
                 Text("\(self.reqresColor.pantone)")
                 Spacer()
             }
             Spacer()
-        }.background(Color(hex: self.reqresColor.code)).edgesIgnoringSafeArea(.all)
-        
+        }.background(ColorCircle(color: Color(hex: self.reqresColor.code)))
+    }
+}
+
+struct ColorCircle: View {
+    let color: Color
+    
+    init(color: Color) {
+        self.color = color
+    }
+    
+    var body: some View {
+        HStack {
+            Spacer()
+            VStack {
+                Spacer()
+                Circle().foregroundColor(.white).edgesIgnoringSafeArea(.all).frame(width: 200, height: 200, alignment: .center).overlay(Circle().stroke(Color.black, lineWidth: 5))
+                Spacer()
+            }
+            Spacer()
+            }.background(color).edgesIgnoringSafeArea(.all)
     }
 }
 
 struct OneColor_Previews: PreviewProvider {
     static var previews: some View {
-        OneColor(reqresColor: ReqresColor(id: 4, name: "Test color", year: 2002, code: "#201010", pantone: "ssdsp"))
+        OneColor(reqresColor: ReqresColor(id: 4, name: "Test color", year: 2002, code: "#B4A1C5", pantone: "ssdsp"))
     }
 }
